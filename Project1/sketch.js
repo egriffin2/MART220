7cdc3be9cf128e8906d3cc8230d46726cae185f3
Jodi Griffin <emily1.griffin@umontana.edu>
2017-09-20 00:19:02 -0600

    added noise

--- Project1/sketch.js
+++ Project1/sketch.js
@@ -12,8 +12,10 @@ function draw() {
   background(135,200,235);
   //var h = 40;
   var h = randomGaussian(100,20);
+  var n = noise(random(100),random(100),random(100));
 
   h = map(h,0,200,50,600);
+  n = map(n,0,.7,0,255); //n (noise output) is mapped to color range
 //Move the ball according to its speed. from Nature of Code
   x = x + xspeed;
   y = y + yspeed;
@@ -31,7 +33,10 @@ function draw() {
 //Display the ball at the location (x,y).
   ellipse(x,y,30,30);
   rect(windowWidth/2,windowHeight-h,40,h,30);
+  fill(n,n,100); //fill is determined by n value
+  ellipse(windowWidth-200,0,300,300);
 
+  //text(n,100,100);
   //text(h,100,100);  checking output of h (commented out because it's ugly)
 
 }
