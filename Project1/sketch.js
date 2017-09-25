16ad3d2affc601573d83b43f4a7c5f92870ad58a
Jodi Griffin <emily1.griffin@umontana.edu>
2017-09-24 21:16:30 -0600

    back to bouncing ball

--- Project1/sketch.js
+++ Project1/sketch.js
@@ -11,12 +11,7 @@ function setup () {
 
 function draw() {
   background(135,200,235);
-  //var h = 40;
-  var h = randomGaussian(100,20);
-  var n = noise(random(100),random(100),random(100));
 
-  h = map(h,0,200,50,600);
-  n = map(n,0,.7,0,255); //n (noise output) is mapped to color range
 //Move the ball according to its speed. from Nature of Code
   x = x + xspeed;
   y = y + yspeed;
@@ -25,19 +20,10 @@ function draw() {
   if ((x > width) || (x < 0)) {
     xspeed = xspeed * -1;
   }
-  if ((y > height) || (y < 600)) {
+  if ((y > height) || (y < 0)) {
     yspeed = yspeed * -1;
   }
 
-  stroke(0);
-  fill(0);
 //Display the ball at the location (x,y).
   ellipse(x,y,30,30);
-  rect(windowWidth/2,windowHeight-h,40,h,30);
-  fill(n,n,100); //fill is determined by n value
-  ellipse(windowWidth-200,0,300,300);
-
-  //text(n,100,100);
-  //text(h,100,100);  checking output of h (commented out because it's ugly)
-
 }
